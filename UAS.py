5a21cb3305b1b79d3f5e10853ce19a9c6075811b
juliantricandra <96005720+juliantricandra@users.noreply.github.com>
2021-12-15 10:38:16 +0700

    Add files via upload

--- UAS.py
+++ UAS.py
@@ -50,8 +50,9 @@ left_col, mid_col, right_col = st.columns(3)
 ## User inputs on the control panel
 st.sidebar.subheader("Pengaturan konfigurasi tampilan")
 list_negara = df['kode_negara'].unique()
+list_tahun = df['tahun'].unique()
 negara = st.sidebar.selectbox("Pilih negara", list_negara)
-
+tahun = st.sidebar.selectbox("Pilih tahun", list_tahun)
 n_tampil = st.sidebar.number_input("Jumlah data yang ditampilkan", min_value=1, max_value=None, value=10)
 ############### sidebar ###############
 
@@ -64,7 +65,7 @@ left_col.dataframe(df.head(n_tampil))
 
 ############### upper middle column ###############
 # Bagian a.
-mid_col.subheader("Grafik jumlah produksi minyak mentah terhadap waktu (tahun) dari suatu negara")
+mid_col.subheader("Jumlah Produksi Minyak Tiap Tahun")
 datanegara = df[df['kode_negara']==negara]
 fig, ax = plt.subplots()
 ax.plot(datanegara['tahun'], datanegara['produksi'], 'go--', linewidth=2, markersize=12)
@@ -75,17 +76,43 @@ mid_col.pyplot(fig)
 ############### upper middle column ###############
 
 ############### upper right column ###############
+# Bagian b. 
+df_b = df[df['tahun']==tahun]
+df_b_sorted = df_b.sort_values(by=['produksi'], ascending=False).reset_index(drop=True)[:n_tampil]
+df_b_sorted.index = df_b_sorted.index + 1
 
+cmap_name = 'tab20c'
+cmap = cm.get_cmap(cmap_name)
+colors = cmap.colors[:len(df_b_sorted['kode_negara'])]
 
+fig, ax = plt.subplots()
+ax.bar(df_b_sorted['kode_negara'],df_b_sorted['produksi'] , color=colors)
+ax.set_xticklabels(df_b_sorted['kode_negara'], rotation=90)
+ax.set_xlabel("Negara", fontsize=12)
+ax.set_ylabel("Jumlah produksi pada tahun %inputjumlahnegara", fontsize=12)
+
+right_col.pyplot(fig)
 ############### upper right column ###############
 
 ############### lower left column ###############
+# Bagian c.
+df_c = df.groupby(by=['kode_negara'])['produksi'].sum().reset_index(name="total_produksi")
+df_c_sorted = df_c.sort_values(by=['total_produksi'],ascending=False).reset_index(drop=True)[:n_tampil]
 
+colors = cmap.colors[:len(df_b_sorted['kode_negara'])]
 
+fig, ax = plt.subplots()
+ax.bar(df_c_sorted['kode_negara'],df_c_sorted['total_produksi'] , color=colors)
+ax.set_xticklabels(df_c_sorted['kode_negara'], rotation=90)
+ax.set_xlabel("Negara", fontsize=12)
+ax.set_ylabel("Total Produksi Keseluruhan Tahun", fontsize=12)
+
+right_col.pyplot(fig)
 ############### lower left column ###############
 
 ############### lower middle column ###############
-
+mid_col.subheader("List Negara")
+mid_col.markdown(negara)
 
 ############### lower middle column ###############
 
