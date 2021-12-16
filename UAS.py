07896f68d63b1e0cb14ba52813743c0e701edc07
juliantricandra <96005720+juliantricandra@users.noreply.github.com>
2021-12-16 14:32:07 +0700

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -121,8 +121,7 @@ with st.container() :
     ax.set_xlabel("Tahun", fontsize=12)
     ax.set_ylabel("Jumlah produksi minyak", fontsize=12)
     with st.expander("Grafik jumlah produksi minyak {} per tahun (a)".format(negara),expanded=False) :
-        #st.pyplot(fig)
-        st.line_chart(datanegara['produksi'])
+        st.pyplot(fig)
         st.dataframe(datanegara)
     
     # Bagian b. 
